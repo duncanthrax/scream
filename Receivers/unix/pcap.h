9be1d25976b3f2f35c8201acb8d0156175550a2d
Can Selcik <selcik.can+githubscream@gmail.com>
2020-05-08 14:46:49 -0700

    libpcap as an audio source + adding shell.nix

---
+++ Receivers/unix/pcap.h
@@ -0,0 +1,51 @@
+#ifndef PCAP_H
+#define PCAP_H
+
+#include "network.h"
+#include "scream.h"
+#include <pcap.h>
+#include <ctype.h>
+
+#define PCAP_BUFSIZ PCAP_BUF_SIZE * 8
+#define SIZE_ETHERNET 14
+
+/* Ethernet addresses are 6 bytes */
+#define ETHER_ADDR_LEN	6
+
+
+struct sniff_ethernet {
+    u_char ether_dhost[ETHER_ADDR_LEN]; /* Destination host address */
+    u_char ether_shost[ETHER_ADDR_LEN]; /* Source host address */
+    u_short ether_type;                 /* IP? ARP? RARP? etc */
+};
+
+struct sniff_ip {
+    u_char ip_vhl;      /* version << 4 | header length >> 2 */
+    u_char ip_tos;      /* type of service */
+    u_short ip_len;     /* total length */
+    u_short ip_id;      /* identification */
+    u_short ip_off;     /* fragment offset field */
+#define IP_RF 0x8000        /* reserved fragment flag */
+#define IP_DF 0x4000        /* dont fragment flag */
+#define IP_MF 0x2000        /* more fragments flag */
+#define IP_OFFMASK 0x1fff   /* mask for fragmenting bits */
+    u_char ip_ttl;      /* time to live */
+    u_char ip_p;        /* protocol */
+    u_short ip_sum;     /* checksum */
+    struct in_addr ip_src,ip_dst; /* source and dest address */
+};
+#define IP_HL(ip)       (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)        (((ip)->ip_vhl) >> 4)
+
+struct sniff_udp {
+    u_short uh_sport;               /* source port */
+    u_short uh_dport;               /* destination port */
+    u_short uh_ulen;                /* udp length */
+    u_short uh_sum;                 /* udp checksum */
+};
+
+int init_pcap(const char* interface_name, int port, char* multicast_group);
+
+int run_pcap(int (*output_function)(receiver_data_t*));
+
+#endif
